0c33b37eff0fec848aa2132ed141fb1102e7b57a
AkshayReddy60 <akshayreddyvangala60@gmail.com>
2024-06-17 10:41:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AWSComponent } from './aws/aws.component';
+import { LinuxComponent } from './linux/linux.component';
+import { SalesforceComponent } from './salesforce/salesforce.component';
+import { AzureComponent } from './azure/azure.component';
+import { FrontendComponent } from './frontend/frontend.component';
+import { ClickComponent } from './click/click.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"aws",component:AWSComponent},
+  {path:"linux",component:LinuxComponent},
+  {path:"salesforce",component:SalesforceComponent},
+  {path:"azure",component:AzureComponent},
+  {path:"frontend",component:FrontendComponent},
+  {path:"click",component:ClickComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AWSComponent } from './aws/aws.component';
+import { LinuxComponent } from './linux/linux.component';
+import { SalesforceComponent } from './salesforce/salesforce.component';
+import { AzureComponent } from './azure/azure.component';
+import { FrontendComponent } from './frontend/frontend.component';
+import { ClickComponent } from './click/click.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AWSComponent,
+    LinuxComponent,
+    SalesforceComponent,
+    AzureComponent,
+    FrontendComponent,
+    ClickComponent,
+   
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,AppRoutingModule,MatToolbarModule,MatIconModule,
+    MatSelectModule,MatOptionModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,
+    MatRadioModule,MatGridListModule,MatCardModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
